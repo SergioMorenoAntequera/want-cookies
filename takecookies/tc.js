2da0a1abf66c53607390fad834d98e04fb6b3b9a
SergioMorenoAntequera <seranmoreno500@gmail.com>
2020-06-06 01:29:45 +0200

    See more dentro

--- takecookies/tc.js
+++ takecookies/tc.js
@@ -3,7 +3,7 @@ class TakeCookies {
     
     constructor(options=null){
         this.tc  = document.querySelector("#takecookies");
-        this.cta = document.querySelector("#takecookies .cta button");
+        this.cta = document.querySelector("#takecookies button");
 
         // Title
         this.tc.appendChild(document.createElement("h1"));
@@ -12,6 +12,8 @@ class TakeCookies {
         } else {
             this.tc.querySelector("h1").innerHTML = "Aviso de cookies";
         }
+
+
 
         // Contenedor
         let content = document.createElement("div");
@@ -26,7 +28,24 @@ class TakeCookies {
         } else {
             text.innerHTML = "Al entrar y navegar por este sitio web usted acepta que se utilicen cookies en su navegador. Estas cookies se van utilizando para poder ofrecer un servicio mejor y más peronalizado, además para recoger información de cara a un sistema de analíticas de la página y no son compartidas con ninguna persona externa a los creadores de la página y el sistema ya mencionado mantenidas por un tiempo indefinido. ";
         }
-        
+
+        // Enlace
+        let link = document.createElement("a");
+        if(options.cookiesPageUrl !== undefined){
+            link.setAttribute("href", options.cookiesPageUrl);
+        } else {
+            link.setAttribute("href", "404");            
+        }
+
+        if(options.cookiesPageText !== undefined){
+            link.innerHTML = options.cookiesPageText;
+        } else {
+            link.innerHTML = "Ver más";
+        }
+        text.appendChild(link);
+
+
+        // text.innerHTML =+ 
 
         
         
@@ -64,4 +83,6 @@ class TakeCookies {
 let cookiesManager = new TakeCookies({
     // title: "Politica de cookies",
     // text: "PRAAA",
+    // cookiesPageText: "Saber más...",
+    // cookiesPageUrl: "Micasa.com/privacidad"
 });
