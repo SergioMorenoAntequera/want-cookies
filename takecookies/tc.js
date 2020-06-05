13ecdfc419cc815737289462cd4cb5f0944503ba
SergioMorenoAntequera <seranmoreno500@gmail.com>
2020-06-06 01:06:31 +0200

    Custom title Done

--- takecookies/tc.js
+++ takecookies/tc.js
@@ -5,18 +5,21 @@ class TakeCookies {
         this.tc  = document.querySelector("#takecookies");
         this.cta = document.querySelector("#takecookies .cta button");
 
-        if(options!==null){
-            if(options.title !== undefined){
-                this.tc.querySelector("h1").innerHTML = options.title;
-            }
-
-            
-            if(options.text !== undefined){
-                this.tc.querySelector(".content p").innerHTML = options.text;
-            } else {
-                this.tc.querySelector(".content p").innerHTML = "Al entrar y navegar por este sitio web usted acepta que se utilicen cookies en su navegador. Estas cookies se van utilizando para poder ofrecer un servicio mejor y más peronalizado, además para recoger información de cara a un sistema de analíticas de la página y no son compartidas con ninguna persona externa a los creadores de la página y el sistema ya mencionado mantenidas por un tiempo indefinido. ";
-            }
+        // Title
+        this.tc.appendChild(document.createElement("h1"));
+        if(options.title !== undefined){
+            this.tc.querySelector("h1").innerHTML = options.title;
+        } else {
+            this.tc.querySelector("h1").innerHTML = "Aviso de cookies";
         }
+
+        
+        if(options.text !== undefined){
+            this.tc.querySelector(".content p").innerHTML = options.text;
+        } else {
+            this.tc.querySelector(".content p").innerHTML = "Al entrar y navegar por este sitio web usted acepta que se utilicen cookies en su navegador. Estas cookies se van utilizando para poder ofrecer un servicio mejor y más peronalizado, además para recoger información de cara a un sistema de analíticas de la página y no son compartidas con ninguna persona externa a los creadores de la página y el sistema ya mencionado mantenidas por un tiempo indefinido. ";
+        }
+        
         
 
         if(this.checkCookies()){
@@ -50,6 +53,7 @@ class TakeCookies {
 }
 
 let cookiesManager = new TakeCookies({
-    title: "Politica de cookies",
-    text: "PRAAA"
+    // title: "Politica de cookies",
+    // text: "PRAAA",
+
 });
