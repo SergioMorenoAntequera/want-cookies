2f38b6700769eb2ab16a1184aada478a9cc47ecf
SergioMorenoAntequera <seranmoreno500@gmail.com>
2020-06-06 15:07:17 +0200

    Giving shape

--- src/tc.js
+++ src/tc.js
@@ -2,104 +2,87 @@
 class TakeCookies {
     
     element;
-    options = [
-        {
-            "name": "title", 
-            "default": "Cookies Warning",
+    options = {
+        title: {
+            "value": "Cookies Warning",
             "selector": "h1",
         },
-        {
-            "name": "text", 
-            "default": "When you enter and browse in this site you are accepting the use of cookies in your browser. This cookies are going to be used to be able to offer a better and custome service, moreover to let us pick information to use used in a analytics system inside this webpage. Also, your cookies are not shared with anyone appart from the responsable group  of the webpage. This cookies are hold for an undefined ammount of time.",
+        text: {
+            "value": "When you enter and browse in this site you are accepting the use of cookies in your browser. This cookies are going to be used to be able to offer a better and custome service, moreover to let us pick information to use used in a analytics system inside this webpage. Also, your cookies are not shared with anyone appart from the responsable group  of the webpage. This cookies are hold for an undefined ammount of time.",
             "selector": "p",
         },
-        {
-            "name": "cookiesPageText", 
-            "default": "See more...",
+        cookiesPageText: {
+            "value": "See more...",
             "selector": "a",
         },
-        {
-            "name": "cookiesPageUrl", 
-            "default": "404",
-            "selector": "a url",
+        cookiesPageUrl: {
+            "value": "404",
+            "prop": "href",
         },
-        {
-            "name": "buttonText", 
-            "default": "OKAY",
+        buttonText: {
+            "value": "OKAY",
             "selector": "button",
         },
-    ];
+    };
     
-    constructor(element=null, options=null){
+    constructor(element = null, options = null){
+        this.setParentElement(element);
+        this.setOptions(options);
         
-
-        this.setElement(element);
-        
-        this.options.forEach(option => {
-            this.setOption(option);
-        });
-
-        // Yeah  you are seeing right
-        if(options==null) { options = {}; }
-        // Title /////////////////////////////////////////////////////////////////////////
-        this.element.appendChild(document.createElement("h1"));
-        if(options.title !== undefined){
-            this.element.querySelector("h1").innerHTML = options.title;
-        } else {
-            this.element.querySelector("h1").innerHTML = "Cookies Warning";
-        }
+        // Titulo
+        this.insertElement(this.element, this.options.title);
 
         // Contenedor ////////////////////////////////////////////////////////////////////
         let content = document.createElement("div");
         content.classList.add("content");
         this.element.appendChild(content);
-
-
-        // Texto
-        let text = document.createElement("p");
-        content.appendChild(text);
-        if(options.text !== undefined){
-            text.innerHTML = options.text;
-        } else {
-            // text.innerHTML = "Al entrar y navegar por este sitio web usted acepta que se utilicen cookies en su navegador. Estas cookies se van utilizando para poder ofrecer un servicio mejor y más peronalizado, además para recoger información de cara a un sistema de analíticas de la página y no son compartidas con ninguna persona externa a los creadores de la página y el sistema ya mencionado mantenidas por un tiempo indefinido. ";
-            text.innerHTML = "When you enter and browse in this site you are accepting the use of cookies in your browser. This cookies are going to be used to be able to offer a better and custome service, moreover to let us pick information to use used in a analytics system inside this webpage. Also, your cookies are not shared with anyone appart from the responsable group  of the webpage. This cookies are hold for an undefined ammount of time.";
-        }
-
-        // Enlace
-        let link = document.createElement("a");
-        if(options.cookiesPageUrl !== undefined){
-            link.setAttribute("href", options.cookiesPageUrl);
-        } else {
-            link.setAttribute("href", "404");            
-        }
-        if(options.cookiesPageText !== undefined){
-            link.innerHTML = options.cookiesPageText;
-        } else {
-            link.innerHTML = "See more...";
-        }
-        text.appendChild(link);
-
-        // Botón //////////////////////////////////////////////////////////////////////////
-        let button = document.createElement("button");
-        if(options.buttonText !== undefined){
-            button.innerHTML = options.buttonText;
-        } else {
-            button.innerHTML = "OKAY";
-        }
-        content.appendChild(button);
-
+        // Texto y botón
+        let texto = this.insertElement(content, this.options.text);
+        let link = this.insertElement(texto, this.options.cookiesPageText);
+        link = this.insetAttribute(link, this.options.cookiesPageUrl);
+        let button = this.insertElement(content, this.options.buttonText);
         
-        this.cta = document.querySelector("#takecookies button");
+
         if(this.checkCookies()){
             this.element.style.visibility = "hidden";
         } else {
-            this.cta.addEventListener("click", (e) => {
+            button.addEventListener("click", (e) => {
                 this.giveCookies();
                 this.element.style.visibility = "hidden";
             });
         }
     }
 
+
+    setParentElement(element){
+        if(!document.querySelector(element)){
+            this.element = document.createElement('div');
+            this.element.setAttribute("id", "takecookies");
+            document.querySelector("body").appendChild(this.element);
+        } else {
+            this.element = document.querySelector(element);
+        }
+    }
+    setOptions(options){
+        let keys = Object.keys(options);
+        let classKeys = Object.keys(this.options);
+        keys.forEach(key => {
+            classKeys.forEach(classKey => {
+                if(key == classKey){
+                    this.options[key].value = options[key];
+                }
+            });
+        });
+    }
+    insertElement(parent, element){
+        let newElement = document.createElement(element.selector)
+        parent.appendChild(newElement);
+        newElement.innerHTML = element.value;
+        return newElement;
+    }
+    insetAttribute(element, atribute){
+       element.setAttribute(atribute.prop, atribute.value); 
+    }
     // COOKIES METHODS
     checkCookies() {
         var name = "wantcookies" + "=";
@@ -119,23 +102,4 @@ class TakeCookies {
     giveCookies() {
         document.cookie = "wantcookies=true";
     }
-
-
-    setElement(element){
-        if(element == null){
-            this.element = document.createElement('div');
-            this.element.setAttribute("id", "takecookies");
-            document.querySelector("body").appendChild(this.element);
-
-        } else {
-            this.element  = document.querySelector(element);
-        }
-    }
-
-    // Check option and put value
-    setOption(option){
-        let optionSelector =  this.element.querySelector(option.selector);
-        console.log(optionSelector);
-    }
-
 }
